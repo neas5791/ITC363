d07ef8a9a7d49daf43909c87e70a9bf04ffd04b5
neAs <neAs@alderaan>
2015-08-19 09:47:20 +1000

    Playing with windings test

--- SPMcode/ex03/canvas_as2.js
+++ SPMcode/ex03/canvas_as2.js
@@ -8,19 +8,15 @@ var NumVertices = 4; // Minimum vertices for polygon
 
 window.onload = function init()
 {
+    //
+    // get the canvas object from the DOM
+    //
     var canvas = document.getElementById( "gl-canvas" );
     
-
     //
-    // Create the gl object
+    // create the GL object and sets the global variable
     //
-    gl = WebGLUtils.setupWebGL( canvas );
-    if ( !gl ) { alert( "WebGL isn't available" ); }
-    //  Configure WebGL
-    gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.clear( gl.COLOR_BUFFER_BIT );
-
+    createGL ( canvas );
 
     //
     // initialize the shaders
@@ -34,33 +30,29 @@ window.onload = function init()
     var bufferId = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
 
-
-
     // Associate out shader variables with our data buffer
-    
     var vPosition = gl.getAttribLocation( program, "vPosition" );
     gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
 
 
-    // Slider handler
+    // Handle the slider event 
     document.getElementById("slider").onchange = function(event) {
         // set variable to slider value
         NumVertices = event.target.value;
         // just a little bit of sconsole action to see how things are going
-        console.log("NumVertices=" + NumVertices);
+        console.log("NumVertices is now " + NumVertices);
     };
 
-    //render();
-//    points = [];
     
     // Mousedown handler
     canvas.addEventListener("mousedown", function(event){
 
         var rect = canvas.getBoundingClientRect();
-        //     vertices[count++] = 
+
         xx = 2*(event.clientX-rect.left)/canvas.width-1;
         yy = 2*(canvas.height-(event.clientY-rect.top))/canvas.height-1;
+
         showLocation(xx, yy);
 
         if (count == 0 ) {
@@ -68,14 +60,19 @@ window.onload = function init()
             gl.clear( gl.COLOR_BUFFER_BIT );
         }
 
-        x = -1 + ( 2 * ( event.offsetX ) / canvas.width );
-        y = -1 + ( 2 * ( ( canvas.height - event.offsetY ) ) / canvas.height);
+        // x = -1 + ( 2 * ( event.offsetX ) / canvas.width );
+        // y = -1 + ( 2 * ( ( canvas.height - event.offsetY ) ) / canvas.height);
 
-        showLocation(x, y);
+        // showLocation(x, y);
 
 
-        point = vec2(x,y);
+        point = vec2(xx,yy);
         // console.log(point + " : " + count);
+
+        if (point.length + 1 % 3 == 0) {
+            var test = point.slice(point.length - 3, point.length - 1);
+            console.log("hi " + RHTwinding( test ));
+        }
 
         points[count++]= point;
 
@@ -119,4 +116,27 @@ function showState() {
 
 function clearState() {
     points = [];
+}
+
+// Tests if the winding is anticlockwise (Right Hand Thumb rule)
+function RHTwinding(vlist) {
+    // Argument is assumed an array of 3 3D points in order P0, P1, P2
+    // Calculate cross product (P1-P0)x(P2-P0)
+    var norm = cross(subtract(vlist[1], vlist[0]), subtract(vlist[2], vlist[0]));
+    return norm[2] >= 0;
+}
+
+
+
+function createGL( canvas ) {
+        //
+    // Create the gl object
+    //
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+    //  Configure WebGL
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear( gl.COLOR_BUFFER_BIT );
+
 }
