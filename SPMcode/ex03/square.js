5de8d05f5a0cbff6121d875c4b7ba591cb8e47a9
neAs <neAs@alderaan>
2015-08-31 15:26:41 +1000

    trying to re align the translation axis after rotation

--- SPMcode/ex03/square.js
+++ SPMcode/ex03/square.js
@@ -2,6 +2,7 @@
 var gl;
 var points;
 
+var colLoc; // fragment shader location
 var matLoc; // shader program location of modelview
 var trans       = [0.0,0.0,0.0]; // displacement of triangle's origin
 var mv; // the transformation matrix
@@ -9,9 +10,12 @@ var theta = 0;
 var vertices;
 var displacementX = [ 0.01, 0, 0 ]; // the positive amount to move the object when translating in X direction
 var displacementY = [ 0, 0.01, 0 ]; // the positive amount to move the object when translating in Y direction
-
+var displacementR = 45.0; // the positive amount to rotate the object about an axis
 var trans2;
 
+
+var CYAN = [0.0,1.0,1.0,1.0];
+var BLACK = [0.0,0.0,0.0,1.0];
 window.onload = function init()
 {
     mv = mat4();
@@ -54,6 +58,7 @@ window.onload = function init()
     gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
 
+    colLoc = gl.getUniformLocation(program, "fColour");
     matLoc = gl.getUniformLocation(program, "modelview");
     window.onkeydown = function( event ) {
         var key = event.keyCode;
@@ -62,11 +67,26 @@ window.onload = function init()
           case 87:
             // w key
             console.log(String.fromCharCode(key));
-            
-            trans = add(trans, displacementY);
-            // mv = mat4();
 
-            mv = mult(mv, translate(displacementY));
+            // float s = sin( theta );
+            // float c = cos( theta );
+
+            // gl_Position.x = -s * vPosition.y + c * vPosition.x;
+            // gl_Position.y =  s * vPosition.x + c * vPosition.y;
+
+
+
+            var tX = -1* Math.sin(theta/2) * displacementY[1];//test
+            var tY = Math.cos(theta/2) * displacementY[1];//test
+
+            var t = [tX, tY, 0];// test
+            
+            trans = add(trans, t);
+
+            // trans = add(trans, displacementY);
+            
+            mv = mult(mv, translate(t));//test
+            // mv = mult(mv, translate(displacementY));
             break;
           case 115:
           case 83:
@@ -100,9 +120,12 @@ window.onload = function init()
           case 113:
           case 81:
             // q key
+            // reset trans to [0,0,0]
             trans = add(trans, scale(-1, trans));
-            mv = mat4();
-            mv = mult(mv, translate(trans));
+            // set mv to be the identity matrix
+            mv = mat4(); // no modifictation to vertices information :)
+            // 
+            //mv = mult(mv, translate(trans));
             console.log(String.fromCharCode(key));
             theta = 0;
             break;
@@ -111,9 +134,9 @@ window.onload = function init()
             // e key
 
             //home(); // sets trans2 to current position
-            theta++;
+            theta = theta+ displacementR;
             var moveCentre = scale(-1,trans);
-            var rotat = rotate(theta, [0,0,1]);
+            var rotat = rotate(displacementR, [0,0,1]);
             var movePo = scale(-1, moveCentre);
 
             mv = mult(mv,translate(moveCentre));
@@ -138,7 +161,7 @@ window.onload = function init()
             mv = mult(mv, translate(temp));
 
 */
-            console.log(String.fromCharCode(key));
+            // console.log(String.fromCharCode(key));
             break;
         }
 
@@ -151,11 +174,13 @@ window.onload = function init()
 
 function render() {
     gl.clear( gl.COLOR_BUFFER_BIT );
-    console.log(trans);
-    // mv = translate(trans);
+    // console.log(trans); // deleted 20150831
+    // mv = translate(trans); // deleted 20150831 don need we set the mv matrix when interaction is handled
     gl.uniformMatrix4fv(matLoc, false, flatten(mv));
-
+    gl.uniform4fv(colLoc, flatten(CYAN));
     gl.drawArrays( gl.TRIANGLE_FAN, 0, 4 );
+    gl.uniform4fv(colLoc, flatten(BLACK));
+    gl.drawArrays (gl.POINTS, 2, 1);
 }
 
 
@@ -177,30 +202,19 @@ function movePolygon(){
     mv = mult(mv, translate(trans));
     render();
 }
+
 function home() {
-     
-    // for ( var i = 0 ; i < vertices.length); i++){
-        
-
-    // }
-    ////console.log("trans is " + trans);
-
-    ////var temp = scale(-1, trans);
-    ////console.log("temp is " + temp);
-
-    ////trans = add(trans, temp);
-    trans2 = trans;
-    trans = [0,0,0];
+    // reset state variables
+    theta = 0;
+    trans = [0,0,0]; // issue 
     mv = mat4();
 
-    // mv = mult(mat4(), mat4());
-    
     render();   
 }
 
 function whereami(){
     console.log("Where am I?");
-    console.log(trans);
+    console.log(trans + " @ " + theta);
     console.log("**************");
 
 }
