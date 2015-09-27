7e0d1c7e0b741e10262c5319e277e88e4a67d517
Sean Matkovich <sean.matkovich@gmail.com>
2015-09-27 12:37:43 +1000

    Tree.js is class implementation of trees used by the scene paths are apainted need to add huts :)

--- SPMcode/AssignmentThree/Tree.js
+++ SPMcode/AssignmentThree/Tree.js
@@ -1,99 +1,87 @@
-// A scene of Stonehenge
-// Using a Block class to represent each stone block
-// Showing use of instance transforms, model-view transformation and perspective projection
-// Combined with scene walk-through
-// Author Ken Lodge 10/09/2015
 // ----------------------------------------------------------------------------------------
 // The Tree class
-// The constructor function for a tree
-// Arguments: a vec3 location and a vec3 scales
+// The constructor function for a block
+// Arguments: a vec3 location, a floating-point angle (degrees) and a vec3 scales
 function Tree(location, scales) {
-	var rs = scalem(scales);	// scale the tree size
-	this.trs = mult(translate(location), rs); // move the tree into location
+    var rs = scalem(scales);
+    this.trs = mult(translate(location), rs);
 }
 
-// A tree's render function
+// A Tree's render function
 // Arguments:
 //   offset - offset of vertices into current vertex attribute array
 //   worldview - current worldview transformation
-Block.prototype.render = function(offset, worldview) {
-	gl.uniformMatrix4fv(mvLoc, false, flatten(mult(worldview, this.trs)));
-	gl.drawArrays(gl.TRIANGLES, offset, Block.NV);
+Tree.prototype.render = function(offset, worldview, colLoc) {
+    gl.uniformMatrix4fv(mvLoc, false, flatten(mult(worldview, this.trs)));
+    gl.uniform4fv(colLoc, flatten(vec4(0.0, 0.37, 0.0, 1.0)));
+    gl.drawArrays(gl.TRIANGLE_FAN, offset, Tree.faces + 2);
+    gl.uniform4fv(colLoc, flatten(vec4(0.398, 0.0, 0.0, 1.0)));
+    gl.drawArrays(gl.TRIANGLES, offset + Tree.faces + 2, Tree.faces * 2 * 3);
 };
 
-// Tree class fields
-// The number of vertices to represent a tree (6 faces x 1 triangle each face)
-Block.NV = 18;	// this should be set when we decide how many faces the tree has
-
+// Block class fields
+// The number of vertices to represent a tree (trunk 6 faces x 2 triangles - 18 vertices & cone 6 faces 
+// in triangle_fan - 8 vertices   )
+Tree.NV = 44;
+Tree.faces = 6;
 // Generator of model vertices - a class method
-// Order is important - It should appear before it is used for Tree.vertices
-Block.initModel = function() {
+// Order is important - It should appear before it is used for Block.vertices
+Tree.initModel = function() {
+    var omega = (360.0 / Tree.faces) * (Math.PI / 180.0);
+    // create the trunk (cylinder shape)
+    var upper = []; 
+    var lower = [];
+    for ( var j = 0; j < Tree.faces ; j++ ){
+        upper.push( vec3( Math.cos( j * omega ) / -6.0, Math.sin( j * omega ) / 6.0, 0.1 ) );
+        lower.push( vec3( Math.cos( j * omega ) / -6.0, Math.sin( j * omega ) / 6.0, 0.0 ) );
+    }
 
-	var nConicalFace = 6;
-	// var nCylinderFace = 6;
-	var conicalVerts = [];
-	var cylinderVerts = [];
-	var theta = ( 360.0 / nConicalFace ) * Math.PI / 180.0; // convert to radians
-	
-	conicalVerts.push( vec4( 0.0, 0.5, 0.0, 1.0));
+    var cylinder = upper.concat(lower);
 
-	for ( var j = 0; j < nConicalFace; j++ ) {
-		conicalVerts[ j + 1 ] = vec4( Math.cos( j * theta) / 2.0, 0.1, Math.sin(j * theta) / 2.0, 1.0 ); // scale the rawVertices
-	}
+    // A local array in which to develop the 36 vertices
+    var vertices = [];
 
-	// number of vertices = 2V + 2 
-	// i.e 6 faces polygon = 
-	//			4V on first face then 2 vertices on each face after that
-	for ( var j = 0; j < (nConicalFace * 2.0) + 2.0; j++ ){
-		if (j % 2 == 0) {
-			cylinderVerts[j] = vec4( Math.cos( j * theta) / 4.0, 0.1, Math.sin(j * theta) / 4.0, 1.0 );
-		}
-		else {
-			// each odd count of j represents a vertices directly below in the y direction
-			cylinderVerts[j] = vec4( Math.cos( (j - 1) * theta) / 4.0, 0.0, Math.sin(j * theta) / 4.0, 1.0 );
-		}
-	}
+    // The 8 raw vertices of the canopy (cone shape)
+    vertices.push( vec3( 0.0, 0.0, 0.5) );
+    for (var i = 0; i < Tree.faces; i++){
+        vertices[i + 1] = vec3( Math.cos( i * omega) / 2.0, Math.sin( i * omega) / 2.0, 0.1 ); // scale the rawVertices
+    }
+    vertices.push(vertices[1]);
 
-	// // The 8 raw vertices of a cube
-	// var rawverts = [
-	// 	vec3(-0.5, -0.5,  0.5),
-	// 	vec3(-0.5,  0.5,  0.5),
-	// 	vec3( 0.5,  0.5,  0.5),
-	// 	vec3( 0.5, -0.5,  0.5),
-	// 	vec3(-0.5, -0.5, -0.5),
-	// 	vec3(-0.5,  0.5, -0.5),
-	// 	vec3( 0.5,  0.5, -0.5),
-	// 	vec3( 0.5, -0.5, -0.5)
-	// ];
-	// A local array in which to develop the 36 vertices
-	var vertices = [];
+    // A nested function generating the vertices for each face
+    function quad(a, b, c, d) {
+        // if abcd is an anticlockwise winding on a face
+        // then abc and acd are anticlockwise windings on its triangles
+        var indices = [a, b, c, a, c, d];
 
-	// A nested function generating the vertices for each face
-	function quad(a, b, c, d) {
-		// if abcd is an anticlockwise winding on a face
-		// then abc and acd are anticlockwise windings on its triangles
-		var indices = [a, b, c, a, c, d];
+        for (var i = 0; i < indices.length; ++i) {
+            vertices.push(cylinder[indices[i]]);
+        }
+    }
 
-		for (var i = 0; i < indices.length; ++i) {
-			vertices.push(rawverts[indices[i]]);
-		}
-	}
+    // A nested function generating the cube's faces
+    function doCube() {
+        // Use anticlockwise windings
+        quad(0,  6,  7, 1);
+        quad(1,  7,  8, 2);
+        quad(2,  8,  9, 3);
+        quad(3,  9, 10, 4);
+        quad(4, 10, 11, 5);
+        quad(5, 11,  6, 0);
+    }
+    // console.log(vertices.length);
+    doCube();
 
-	// A nested function generating the cube's faces
-	function doCube() {
-		// Use anticlockwise windings
-		quad(1, 0, 3, 2);
-		quad(2, 3, 7, 6);
-		quad(3, 0, 4, 7);
-		quad(6, 5, 1, 2);
-		quad(4, 5, 6, 7);
-		quad(5, 4, 0, 1);
-	}
-
-	doCube();
-	return vertices;
+    // for (var j = 0; j < vertices.length; j++ ) {
+    //     console.log(vertices[j]);
+    // }
+    // console.log(vertices.length);
+    
+    // the vertices array contains 8 vertices for the 
+    // GL.TRIANGLE_FAN and 36 vertices for the GL.TRIANGLE
+    return vertices;
 }
 
 // The model vertices - a class field
-Block.vertices = Block.initModel();
+Tree.vertices = Tree.initModel();
 //----------------------------------------------------------------------------
--- SPMcode/AssignmentThree/stonehenge2.js
+++ SPMcode/AssignmentThree/stonehenge2.js
@@ -1,89 +1,92 @@
-// A scene of a park
-// Using a Tree class to represent each tree in the scene
-// ----------------------------------------------------------------------------------------
-// The Tree class
-// The constructor function for a block
-// Arguments: a vec3 location, a floating-point angle (degrees) and a vec3 scales
-function Tree(location, scales) {
-    var rs = scalem(scales);
-    this.trs = mult(translate(location), rs);
-}
+// // A scene of a park
+// // Using a Tree class to represent each tree in the scene
+// // ----------------------------------------------------------------------------------------
+// // The Tree class
+// // The constructor function for a block
+// // Arguments: a vec3 location, a floating-point angle (degrees) and a vec3 scales
+// function Tree(location, scales) {
+//     var rs = scalem(scales);
+//     this.trs = mult(translate(location), rs);
+// }
 
-// A Tree's render function
-// Arguments:
-//   offset - offset of vertices into current vertex attribute array
-//   worldview - current worldview transformation
-Tree.prototype.render = function(offset, worldview) {
-    gl.uniformMatrix4fv(mvLoc, false, flatten(mult(worldview, this.trs)));
-    gl.drawArrays(gl.TRIANGLE_FAN, offset, Tree.faces + 2);
-    gl.drawArrays(gl.TRIANGLES, offset + Tree.faces + 2, Tree.faces * 2 * 3);
-};
+// // A Tree's render function
+// // Arguments:
+// //   offset - offset of vertices into current vertex attribute array
+// //   worldview - current worldview transformation
+// Tree.prototype.render = function(offset, worldview, colLoc) {
+//     gl.uniformMatrix4fv(mvLoc, false, flatten(mult(worldview, this.trs)));
+//     gl.uniform4fv(colLoc, flatten(vec4(0.0, 0.37, 0.0, 1.0)));
+//     gl.drawArrays(gl.TRIANGLE_FAN, offset, Tree.faces + 2);
+//     gl.uniform4fv(colLoc, flatten(vec4(0.398, 0.0, 0.0, 1.0)));
+//     gl.drawArrays(gl.TRIANGLES, offset + Tree.faces + 2, Tree.faces * 2 * 3);
+// };
 
-// Block class fields
-// The number of vertices to represent a tree (trunk 6 faces x 2 triangles - 18 vertices & cone 6 faces 
-// in triangle_fan - 8 vertices   )
-Tree.NV = 44;
-Tree.faces = 6;
-// Generator of model vertices - a class method
-// Order is important - It should appear before it is used for Block.vertices
-Tree.initModel = function() {
-    var omega = (360.0 / Tree.faces) * (Math.PI / 180.0);
-    // create the trunk (cylinder shape)
-    var upper = []; 
-    var lower = [];
-    for ( var j = 0; j < Tree.faces ; j++ ){
-        upper.push( vec3( Math.cos( j * omega ) / -6.0, Math.sin( j * omega ) / 6.0, 0.1 ) );
-        lower.push( vec3( Math.cos( j * omega ) / -6.0, Math.sin( j * omega ) / 6.0, 0.0 ) );
-    }
+// // Block class fields
+// // The number of vertices to represent a tree (trunk 6 faces x 2 triangles - 18 vertices & cone 6 faces 
+// // in triangle_fan - 8 vertices   )
+// Tree.NV = 44;
+// Tree.faces = 6;
+// // Generator of model vertices - a class method
+// // Order is important - It should appear before it is used for Block.vertices
+// Tree.initModel = function() {
+//     var omega = (360.0 / Tree.faces) * (Math.PI / 180.0);
+//     // create the trunk (cylinder shape)
+//     var upper = []; 
+//     var lower = [];
+//     for ( var j = 0; j < Tree.faces ; j++ ){
+//         upper.push( vec3( Math.cos( j * omega ) / -6.0, Math.sin( j * omega ) / 6.0, 0.1 ) );
+//         lower.push( vec3( Math.cos( j * omega ) / -6.0, Math.sin( j * omega ) / 6.0, 0.0 ) );
+//     }
 
-    var cylinder = upper.concat(lower);
+//     var cylinder = upper.concat(lower);
 
-    // A local array in which to develop the 36 vertices
-    var vertices = [];
+//     // A local array in which to develop the 36 vertices
+//     var vertices = [];
 
-    // The 8 raw vertices of the canopy (cone shape)
-    vertices.push( vec3( 0.0, 0.0, 0.5) );
-    for (var i = 0; i < Tree.faces; i++){
-        vertices[i + 1] = vec3( Math.cos( i * omega) / 2.0, Math.sin( i * omega) / 2.0, 0.1 ); // scale the rawVertices
-    }
-    vertices.push(vertices[1]);
+//     // The 8 raw vertices of the canopy (cone shape)
+//     vertices.push( vec3( 0.0, 0.0, 0.5) );
+//     for (var i = 0; i < Tree.faces; i++){
+//         vertices[i + 1] = vec3( Math.cos( i * omega) / 2.0, Math.sin( i * omega) / 2.0, 0.1 ); // scale the rawVertices
+//     }
+//     vertices.push(vertices[1]);
 
-    // A nested function generating the vertices for each face
-    function quad(a, b, c, d) {
-        // if abcd is an anticlockwise winding on a face
-        // then abc and acd are anticlockwise windings on its triangles
-        var indices = [a, b, c, a, c, d];
+//     // A nested function generating the vertices for each face
+//     function quad(a, b, c, d) {
+//         // if abcd is an anticlockwise winding on a face
+//         // then abc and acd are anticlockwise windings on its triangles
+//         var indices = [a, b, c, a, c, d];
 
-        for (var i = 0; i < indices.length; ++i) {
-            vertices.push(cylinder[indices[i]]);
-        }
-    }
+//         for (var i = 0; i < indices.length; ++i) {
+//             vertices.push(cylinder[indices[i]]);
+//         }
+//     }
 
-    // A nested function generating the cube's faces
-    function doCube() {
-        // Use anticlockwise windings
-        quad(0,  6,  7, 1);
-        quad(1,  7,  8, 2);
-        quad(2,  8,  9, 3);
-        quad(3,  9, 10, 4);
-        quad(4, 10, 11, 5);
-        quad(5, 11,  6, 0);
-    }
-    console.log(vertices.length);
-    doCube();
+//     // A nested function generating the cube's faces
+//     function doCube() {
+//         // Use anticlockwise windings
+//         quad(0,  6,  7, 1);
+//         quad(1,  7,  8, 2);
+//         quad(2,  8,  9, 3);
+//         quad(3,  9, 10, 4);
+//         quad(4, 10, 11, 5);
+//         quad(5, 11,  6, 0);
+//     }
+//     // console.log(vertices.length);
+//     doCube();
 
-    for (var j = 0; j < vertices.length; j++ ) {
-        console.log(vertices[j]);
-    }
-    console.log(vertices.length);
-    // the vertices array contains 8 vertices for the 
-    // GL.TRIANGLE_FAN and 36 vertices for the GL.TRIANGLE
-    return vertices;
-}
+//     // for (var j = 0; j < vertices.length; j++ ) {
+//     //     console.log(vertices[j]);
+//     // }
+//     // console.log(vertices.length);
 
-// The model vertices - a class field
-Tree.vertices = Tree.initModel();
-//----------------------------------------------------------------------------
+//     // the vertices array contains 8 vertices for the 
+//     // GL.TRIANGLE_FAN and 36 vertices for the GL.TRIANGLE
+//     return vertices;
+// }
+
+// // The model vertices - a class field
+// Tree.vertices = Tree.initModel();
+// //----------------------------------------------------------------------------
 
 
 
@@ -112,22 +115,42 @@ const HUT = vec4(0.0, 0.0, 1.0 , 1.0); // 0000ff - blue
 
 //  Ground vertices for a 2000m x 2000m triangle fan
 var ground = [
-    vec3(1000.0, -1000.0, 0.0),
-    vec3(1000.0, 1000.0, 0.0),
-    vec3(-1000.0, 1000.0, 0.0),
+    vec3( 1000.0, -1000.0, 0.0),
+    vec3( 1000.0,  1000.0, 0.0),
+    vec3(-1000.0,  1000.0, 0.0),
     vec3(-1000.0, -1000.0, 0.0)
 ];
+
+
+//  Path vertices through the grass area
+var pathWidth = 5.0;
+
+var path = [
+    vec3( pathWidth, -1000.0, 0.1),
+    vec3( pathWidth,  1000.0, 0.1),
+    vec3(-pathWidth,  1000.0, 0.1),
+    vec3(-pathWidth, -1000.0, 0.1),
+];
+
+var path2 = [
+    vec3( 1000.0, -pathWidth, 0.1),
+    vec3( 1000.0,  pathWidth, 0.1),
+    vec3(-1000.0,  pathWidth, 0.1),
+    vec3(-1000.0, -pathWidth, 0.1),
+];
+
 var NVground = 4;   // Number of ground vertices
+var NVpath = 4;
 
 // Stonehenge parameters (lengths in metres)
-const RINGRAD = 16.5;   // Radius of ring
-const SSH = 4.1;        // Standing stone height (above ground)
-const SSW = 2.1;        // Standing stone width
-const SST = 1.1;        // Standing stone thickness
-const LSH = 0.8;        // Lintel stone height
-const LSW = 3.2;        // Lintel stone width (aka length)
-const LST = 1.0;        // Lintel stone thickness
-const NST = 30;         // Number of stones of each types (total = 2*NST)
+// const RINGRAD = 16.5;   // Radius of ring
+// const SSH = 4.1;        // Standing stone height (above ground)
+// const SSW = 2.1;        // Standing stone width
+// const SST = 1.1;        // Standing stone thickness
+// const LSH = 0.8;        // Lintel stone height
+// const LSW = 3.2;        // Lintel stone width (aka length)
+// const LST = 1.0;        // Lintel stone thickness
+const NST = 300;         // Number of standing trees
 
 // Arrays of Trees objects representing the standing stones and the lintels
 var trees = [];
@@ -156,9 +179,16 @@ window.onload = function init() {
 
     var vBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, sizeof['vec3']*(NVground+Tree.NV), gl.STATIC_DRAW);
+    // buffer needs to be big enough to keep grass, two paths, all the trees & a couple of huts
+    gl.bufferData(gl.ARRAY_BUFFER, sizeof['vec3']*(NVground+NVpath+NVpath+Tree.NV), gl.STATIC_DRAW);
+    // put the ground into the buffer
     gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(ground));
-    gl.bufferSubData(gl.ARRAY_BUFFER, sizeof['vec3']*NVground, flatten(Tree.vertices));
+    // put a path into the buffer
+    gl.bufferSubData(gl.ARRAY_BUFFER, sizeof['vec3']*NVground, flatten(path));
+    // put the second path into the buffer
+    gl.bufferSubData(gl.ARRAY_BUFFER, sizeof['vec3']*(NVground+NVpath), flatten(path2));
+    // put the trees into the buffer
+    gl.bufferSubData(gl.ARRAY_BUFFER, sizeof['vec3']*(NVground+(2*NVpath)), flatten(Tree.vertices));
 
     var vPosition = gl.getAttribLocation(program, "vPosition");
     gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
@@ -241,39 +271,86 @@ function render() {
     gl.uniformMatrix4fv(mvLoc, false, flatten(worldview));
     gl.uniform4fv(colLoc, flatten(GRASS));
     gl.drawArrays(gl.TRIANGLE_FAN, 0, NVground);
-
+    gl.uniform4fv(colLoc, flatten(PATH));
+    gl.drawArrays(gl.TRIANGLE_FAN, NVground, NVpath);
+    gl.drawArrays(gl.TRIANGLE_FAN, NVground+NVpath, NVpath);
     // Stones in model coordinates need modelview = worldview*TRS
-    gl.uniform4fv(colLoc, flatten(TREE));
+    // gl.uniform4fv(colLoc, flatten(TREE));
     for (var i = 0; i < NST; i++) {
-        trees[i].render(NVground, worldview);
+        trees[i].render(NVground+NVpath+NVpath, worldview, colLoc);
     }
 }
 
 function doTrees() {
     // Generate tree array
-    //var scales = vec3(SST, SSW, SSH);   // stone faces along x-direction
-    var scales = vec3( 2.1, 2.1, 20.0);   // stone faces along x-direction
-
-    var dtheta = 2*Math.PI/NST;         // angle change per stone in radians
-    var theta;
+    var scales = vec3( 2.1, 2.1, 20.0);   // scale the trees
     var location;
+
+    var unique = uniqueLocations(300, 100);
+
     for (var i = 0; i < NST; i++) {
-        theta = i*dtheta;
-
-        for (var i = 0; i < 200; i++){
-            var x = ( Math.random() * 2000 ) - 1000;
-            var y = ( Math.random() * 2000 ) - 1000;
-            var z = 0;            
-        }
-
-        location = vec3(RINGRAD*Math.cos(theta), RINGRAD*Math.sin(theta), 0);//0.5*SSH
+        location = unique[i];
         trees[i] = new Tree(location, scales);
     }
-    // // Generate lintels array
-    // scales = vec3(LST, LSW, LSH);   // stone lies along y-direction
-    // for (i = 0; i < NST; i++) {
-    //     theta = (i + 0.5)*dtheta;   // includes offset angle upright to lintel
-    //     location = vec3(RINGRAD*Math.cos(theta), RINGRAD*Math.sin(theta), SSH + 0.5*LSH);
-    //     lintels[i] = new Block(location, theta*180.0/Math.PI, scales);
+    console.log(trees.length);
+}
+
+/* Creating an array of unique vec3 positions
+ * 
+*/
+function uniqueLocations(numberOfLocations, area){
+    
+    var unique = [];
+
+    function test(x){
+        if (x > -pathWidth*1.2 && x < pathWidth*1.2){
+            // console.log("found a false!");
+            return false;
+
+        }
+
+        return true;
+    }
+
+    do {
+        var x = ( Math.random() * area ) - (area / 2);
+        var y = ( Math.random() * area ) - (area / 2);
+        var z = 0;
+        var v = vec3(x, y, z);
+        if (unique.length == 0) {
+            unique.push(v);
+        }
+        else
+        {
+            if (test(x) && test(y))
+                if (unique.indexOf(v) < 0 )
+                    unique.push(v);
+        }
+    } while (unique.length < numberOfLocations);
+
+    console.log(unique.length);
+
+    // for (var i = 0; i < numberOfLocations; i++){
+    //     var x = ( Math.random() * area ) - (area / 2);
+    //     var y = ( Math.random() * area ) - (area / 2);
+    //     var z = 0;
+    //     var v = vec3(x, y, z);
+    //     if (i == 0) {
+    //         unique.push(v);
+    //     }
+    //     else
+    //     {
+    //         if (test(x) && test(y))
+    //             if (unique.indexOf(v) < 0 )
+    //                 unique.push(v);
+    //             else
+    //                 i--;
+    //     }
     // }
+
+
+
+    // for (var i = 0 ; i < unique.length; i++)
+    //     console.log(unique[i]);
+    return unique;
 }
