d5b548305fc0a62b3bb9bc599a3c9b0dd9b82ec1
neAs <neAs@alderaan>
2015-10-29 15:22:15 +1100

    object viewer working

--- SPMcode/AssignmentThree/ITC363_ObjViewer.js
+++ SPMcode/AssignmentThree/ITC363_ObjViewer.js
@@ -5,26 +5,11 @@
 
 var gl;             // the gl context object
 
-var faces = 20
-;
+var faces = 20;
 
-
-var n;              // the number of divisions map out
-var nRows;          // used for array to store x co ordinates
-var nColumns;       // used for array to stor y co ordinate
-var unit;           // as we map the plot from -1 < x < 1 && -1 < y < 1 our array starts at the exteme
-                    // of x = -1 and y = -1  effective unit constant is double the inverse of division
-
-var data = [];  // data is a two dimesnional array which store the z values for the respective 
-                // x and y inputs.
 
 var pointsArray = [];   // the array that will store the vertices information to be sent to buffer
 
-var xAxis;      // position in the array where the x Axis vertices start
-var yAxis;      // position in the array where the y Axis vertices start
-var zAxis;      // position in the array where the z Axis vertices start
-
-var fColor;     // vertex shader link variable
 
 var near   = -10 ; // initial position for near altered by increasing and decreasing the z buttons
 var far    =  10 ;   // initial position for far  altered by increasing and decreasing the z buttons
@@ -38,8 +23,8 @@ var dr     =   5.0 * Math.PI / 180.0 ;   // the amount to alter theta and phi
 const black = vec4(0.0, 0.0, 0.0, 1.0);
 const lgreen = vec4(0.37, 0.68, 0.53);
 const red = vec4(1.0, 0.0, 0.0, 1.0);
-const blue = vec4(0.0, 1.0, 0.0, 1.0);
-const green = vec4(0.0, 0.0, 1.0, 1.0);
+const blue = vec4(0.0, 0.0, 1.0, 1.0);
+const green = vec4(0.0, 1.0, 0.0, 1.0);
 const yellow = vec4(1.0, 1.0, 0.0, 1.0);
 const grey =  vec4(0.539, 0.539, 0.539, 1.0);
 /* ******************************************** */
@@ -64,7 +49,6 @@ window.onload = function init()
 
     gl.viewport( 0, 0, canvas.width, canvas.height );
     gl.clearColor(0.6, 0.8, 1.0, 1.0);      // Light blue background for sky    
-    // gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
     
     // enable depth testing and polygon offset
     // so lines will be in front of filled triangles
@@ -74,9 +58,7 @@ window.onload = function init()
     gl.enable(gl.POLYGON_OFFSET_FILL);
     gl.polygonOffset(1.0, 2.0);
 
-    //
     //  Load shaders and initialize attribute buffers
-    //
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
     
@@ -96,11 +78,10 @@ window.onload = function init()
 
 
     // get the number vertices to display from the user interface html slider
+    // setVerticesCount(document.getElementById("vertex-slider").value);
 
-    setVerticesCount(document.getElementById("vertex-slider").value);
-
-    var cone = makeCone(faces);
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(cone), gl.STATIC_DRAW); // push the new pointArray to the buffer
+    var tree = makeTree(faces);
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(tree), gl.STATIC_DRAW); // push the new pointArray to the buffer
 // buttons for moving viewer and changing size 
 
     document.getElementById("Button1").onclick = function()  { near   *= 1.1 ; far    *= 1.1 ; render() ; };
@@ -117,147 +98,11 @@ window.onload = function init()
     document.getElementById("Button12").onclick = function() { ytop   *= 1.1 ; bottom *= 1.1 ; render() ; };
 
 
-    document.getElementById("vertex-slider").onchange = function(event) { setVerticesCount(event.target.value); render() ; };
+    // document.getElementById("vertex-slider").onchange = function(event) { setVerticesCount(event.target.value); render() ; };
 
        
     render();
  
-}
-
-
-// outputs vertices to produce triangle fan in cone shape
-// 
-function makeCone(nfaces)
-{
-    // var faces = nfaces;
-    var omega = (360.0 / faces) * (Math.PI / 180.0);
-    var vertices = [];
-
-
-    // The 8 raw vertices of the canopy (cone shape)
-    vertices.push( vec4( 0.0, 0.0, 0.5 , 1) );
-    for (var i = 0; i < faces; i++){
-        vertices[i + 1] = vec4( Math.cos( i * omega) / 2.0, Math.sin( i * omega) / 2.0, 0.1, 1 ); // scale the rawVertices
-    }
-    vertices.push(vertices[1]);
-
-    for (var j = 0 ; j < vertices.length; j++) {
-        console.log(vertices[j]);
-    }
-
-    return vertices;
-}
-
-
-
-
-
-/*
- * Set the state variables of the mesh based on the number of vertices
- *
- * value    is the number of vertices to calculate in each of the x and y directions  
-*/
-function setVerticesCount(value)
-{
-    console.log("In setVerticesCount()");
-
-    n = value;
-    nRows = n;      // used for array to store x co ordinates
-    nColumns = n;   // used for array to stor y co ordinate
-    unit = 2.0 / n; // as we map the plot from -1 < x < 1 && -1 < y < 1 our array starts at the exteme
-                    // of x = -1 and y = -1  effective unit constant is double the inverse of division
-
-    // buildDataArray();   // calculate and produce vertices data array
-    // buildPointArray();  // arrange data array values into ordered vetices list
-
-    // gl.bufferData( gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW); // push the new pointArray to the buffer
-}
-
-
-/*
- * Creates the data[][] which contains the z vertex information for each paired set of x & y values
-*/
-function buildDataArray() 
-{
-    console.log("In buildDataArray()");
-    data = []; // create a new data array
-
-    for( var i = 0; i < nRows; ++i ) {
-        // initialize multi dimensional array by pushings and array
-        data.push( [] );
-        // set the value of x with respect the number of vertices (n)
-        // and the scale value (unit)
-        var x = (i - ( n / 2 ) ) * unit;
-
-        for( var j = 0; j < nColumns; ++j ) {
-            // set the value of y with respect the number of vertices (n)
-            // and the scale value (unit)
-            var y = (j - ( n / 2 ) ) * unit;
-
-            // set the value of z based on the function f(x,y) = x^2 + y^2
-            var z = x*x-y*y;
-            
-            // push the z value to the array
-            data[i][j] = z;
-        }
-    }
-}
-
-
-/*
- * Orders the verices information produced by the buildDataArray() into a list of 
- * vec4 values such that quadrilateral line loops are formed when rendered.
-*/
-function buildPointArray() 
-{
-    console.log("In buildPointArray()");
-    pointsArray = []; // create a new pointsArray
-    // vertex array of nRows*nColumns quadrilaterals 
-    // (two triangles/quad) from data
-    
-    for(var i=0; i<nRows-1; i++) {
-        for(var j=0; j<nColumns-1;j++) {
-            // push the vetices into the pointsArray in the correct order
-            pointsArray.push( vec4 ( unit * ( i - ( n / 2 ) )
-                                    , data[i][j]
-                                    , unit * ( j - ( n / 2 ) )
-                                    , 1.0));
-
-            pointsArray.push( vec4 ( unit * ( ( i + 1 ) - ( n / 2 ) )
-                                    , data[ i + 1 ][ j ]
-                                    , unit * ( j - ( n / 2 ) )
-                                    , 1.0)); 
-
-            pointsArray.push( vec4 ( unit * ( ( i + 1 ) - ( n / 2 ) )
-                                    , data[ i + 1 ][ j + 1 ]
-                                    , unit * ( ( j + 1 ) - ( n / 2 ) )
-                                    , 1.0));
-
-            pointsArray.push( vec4 ( unit * ( i - ( n / 2 ) )
-                                    , data[ i ][ j + 1 ]
-                                    , unit * ( ( j + 1 ) - ( n / 2 ) )
-                                    , 1.0) );
-        }
-    }
-
-    // Set marker for the end of the vertex data and 
-    // start of the x axis vertices
-    xAxis = pointsArray.length;
-
-    pointsArray.push( vec4(-1.0, 0.0, 0.0, 1) )
-    pointsArray.push( vec4( 1.0, 0.0, 0.0, 1) )
-
-    // Set marker for the start of the y axis vertices
-    yAxis = pointsArray.length;
-
-    pointsArray.push( vec4( 0.0, -1.0, 0.0, 1) )
-    pointsArray.push( vec4( 0.0,  1.0, 0.0, 1) )
-
-    // Set marker for the start of the z axis vertices
-    zAxis = pointsArray.length;
-
-    pointsArray.push( vec4( 0.0, 0.0, -1.0, 1) )
-    pointsArray.push( vec4( 0.0, 0.0,  1.0, 1) )
 }
 
 
@@ -292,35 +137,46 @@ function render()
     gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
     gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
 
-        gl.uniform4fv(fColor, flatten(yellow));
-        gl.drawArrays( gl.TRIANGLE_FAN, 0, faces + 2 );
-    
-    // for (var i = 0; i < faces; i ) {
-        // gl.drawArrays( gl.LINE_LOOP, 0, 3 );
-    // }
-    // // draw each quad as two filled yellow triangles
-    // // and then as two black line loops
-    // for( var i = 0; i < pointsArray.length - 6; i += 4 ) { 
-    //     gl.uniform4fv(fColor, flatten(yellow));
-    //     gl.drawArrays( gl.TRIANGLE_FAN, i, 4 );
-
-    //     // change the LINE_LOOP colour based on the number of vertices displayed in the mesh
-    //     if (n > 50 )
-    //         gl.uniform4fv(fColor, flatten(grey));
-    //     else
-    //         gl.uniform4fv(fColor, flatten(black));
-
-    //     gl.drawArrays( gl.LINE_LOOP, i, 4 );
-    // }
-
-    // Draw the axis markers 
-    // x axis red
-    gl.uniform4fv(fColor, flatten(red));
-    gl.drawArrays( gl.LINES, xAxis, 2);
-    // y axis green
     gl.uniform4fv(fColor, flatten(green));
-    gl.drawArrays( gl.LINES, yAxis, 2);
-    // z axis blue
-    gl.uniform4fv(fColor, flatten(blue));
-    gl.drawArrays( gl.LINES, zAxis, 2);
+    gl.drawArrays( gl.TRIANGLE_FAN, 0, faces + 2 );
+    gl.uniform4fv(fColor, flatten(black));
+    gl.drawArrays( gl.TRIANGLE_STRIP, faces + 4, (faces*2) + 2 );
+}
+
+
+
+// outputs vertices to produce triangle fan in cone shape
+function makeTree(nfaces)
+{
+    var vCone = nfaces + 2;
+    var omega = (360.0 / faces) * (Math.PI / 180.0);
+    var vertices = [];
+
+    // The 8 raw vertices of the canopy (cone shape)
+    vertices.push( vec4( 0.0, 0.0, 0.5 , 1) );
+    for (var i = 0; i < faces; i++){
+        vertices[i + 1] = vec4( Math.cos( i * omega) / 2.0, Math.sin( i * omega) / 2.0, 0.1, 1 ); // scale the rawVertices
+    }
+
+    // push the first point of the fan to complete the cone
+    vertices.push(vertices[1]);
+
+    var vCylinder = nfaces * 2;
+
+    for (var j = 0 ; j < faces*2; j++) {
+        if (j == 0 ) {
+            vertices.push( vec4( Math.cos( j * omega) / 2.0, Math.sin( j * omega) / 2.0, 0.1, 1));
+            vertices.push( vec4( Math.cos( j * omega) / 2.0, Math.sin( j * omega) / 2.0, 0.0, 1));
+        }
+        
+        vertices.push( vec4( Math.cos( (j + 1) * omega) / 6.0, Math.sin( (j + 1) * omega) / 6.0, 0.1, 1));
+        vertices.push( vec4( Math.cos( (j + 1) * omega) / 6.0, Math.sin( (j + 1) * omega) / 6.0, 0.0, 1));
+    }
+
+
+    for (var j = 0 ; j < vertices.length; j++) {
+        console.log(vertices[j]);
+    }
+
+    return vertices;
 }
